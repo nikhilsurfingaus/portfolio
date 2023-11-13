be197e49b094d3faf183654f8987e4e9e17b5ed2
Nikhil Naik <46309727+nikhilsurfingaus@users.noreply.github.com>
2023-11-13 17:21:27 +1100

    Refactor: Code quality Skills, moved static code date into seperate js data file code quality improv

---
+++ src/components/SkillsUI/SkillsData.js
@@ -0,0 +1,25 @@
+export const hardSkills = [
+    { name: "React TSX", value: 95 },
+    { name: "Javascript", value: 90 },
+    { name: "Python", value: 85 },
+    { name: "CSS", value: 78 },
+    { name: "Typescript", value: 90 },
+    { name: "Solidity/Web3", value: 85 },
+    { name: "Vue TS", value: 72 },
+    { name: "Django", value: 78 },
+    { name: "Java", value: 82 },
+    { name: "OracleSQL", value: 80 },
+];
+
+export const softSkills = [
+    { name: "Collaboration", value: 90 },
+    { name: "Goal-Orientated", value: 88 },
+    { name: "Problem Solving", value: 85 },
+    { name: "Empathy", value: 86 },
+    { name: "Chat GPT", value: 96 },
+    { name: "Time Management", value: 88 },
+    { name: "Creativity", value: 96 },
+    { name: "Adaptability", value: 88 },
+    { name: "Positivity", value: 96 },
+    { name: "UI/UX Design", value: 95 },
+];
